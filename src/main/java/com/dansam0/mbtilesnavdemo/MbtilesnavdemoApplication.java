7a022bfc747552c7591618e402dbb744015cd2f6
DanSam0 <Dansamo0114@gmail.com>
2025-04-19 16:20:12 +0300

    Initial commit

---
+++ src/main/java/com/dansam0/mbtilesnavdemo/MbtilesnavdemoApplication.java
@@ -0,0 +1,13 @@
+package com.dansam0.mbtilesnavdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtilesnavdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtilesnavdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dansam0/mbtilesnavdemo/MbtilesnavdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dansam0.mbtilesnavdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtilesnavdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
